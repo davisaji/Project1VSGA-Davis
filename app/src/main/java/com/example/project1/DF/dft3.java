db1ba40f1eedba6acd24739fb162739cc8117abb
Davis Aji <dvsprwj@gmail.com>
2020-10-04 13:58:36 +0700

    Membuat ClassTAB

---
+++ app/src/main/java/com/example/project1/DF/dft3.java
@@ -0,0 +1,20 @@
+package com.example.project1.DF;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.example.project1.R;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class dft3 extends Fragment {
+
+    @Nullable
+    @Override
+    public android.view.View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.df_t3, container, false);
+    }
+}

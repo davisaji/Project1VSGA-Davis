90a366e08ff5c0642042594081aa4df4df1a79ed
Davis Aji <dvsprwj@gmail.com>
2020-10-07 10:28:00 +0700

    Finish1000%

--- app/src/main/java/com/example/project1/Beranda.java
+++ app/src/main/java/com/example/project1/Beranda.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.project1.Model.DMModel;
 
@@ -22,5 +23,13 @@ public class Beranda extends AppCompatActivity {
         Intent i = new Intent(Beranda.this, DaftarMenu.class);
         startActivity(i);
         finish();
+    }
+
+    public void riwayatOrde(View view) {
+        Toast.makeText(this,"On Progres", Toast.LENGTH_LONG).show();
+    }
+
+    public void OrderNew(View view) {
+        Toast.makeText(this, "On Progres", Toast.LENGTH_LONG).show();
     }
 }
